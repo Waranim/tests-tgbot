c829dc69a24463b2877042a069741dd434c55a9d
Waranim <129619485+Waranim@users.noreply.github.com>
2024-10-27 11:45:59 +0500

    Init commit

---
+++ src/main/java/org/example/naumenteststgbot/NaumenTestsTgbotApplication.java
@@ -0,0 +1,13 @@
+package org.example.naumenteststgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenTestsTgbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaumenTestsTgbotApplication.class, args);
+    }
+
+}
