c339bd5d67e9521461be5003332eaba86648bf45
ntrofim20 <ntrofim35@mail.ru>
2024-12-05 20:38:51 +0500

    Написал тесты

--- src/test/java/org/example/naumenteststgbot/service/TestServiceTest.java
+++ src/test/java/org/example/naumenteststgbot/service/TestServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,6 +37,16 @@ class TestServiceTest {
     private UserService userService;
 
     /**
+     * Создание сообщений
+     */
+    private MessageBuilder messageBuilder;
+
+    /**
+     * Сервис для создания inline клавиатур
+     */
+    @Mock
+    private KeyboardService keyboardService;
+    /**
      * Сервис тестов
      */
     @InjectMocks
@@ -62,13 +73,22 @@ class TestServiceTest {
     private UserSession userSession;
 
     /**
+     * Идентификатор чата
+     */
+    private String chatId;
+
+
+    /**
      * Инициализация перед каждым тестом
      */
     @BeforeEach
     void setUp() {
+        chatId = "1234567";
         userId = 123L;
         testId = 123L;
         test = new TestEntity(userId);
+        messageBuilder = new MessageBuilder();
+        testService = new TestService(testRepository, userService , keyboardService , messageBuilder);
         test.setTitle("Название теста");
         ReflectionTestUtils.setField(test, "id", testId);
         userSession = new UserSession(userId);
@@ -80,11 +100,6 @@ class TestServiceTest {
         questionEntity.getAnswers().add(answer);
         questionEntity.setQuestion("Вопрос");
         test.getQuestions().add(questionEntity);
-
-        when(userService.getSession(userId)).thenReturn(userSession);
-        when(userService.getTestsById(userId)).thenReturn(List.of(test));
-        when(testRepository.findById(testId)).thenReturn(Optional.of(test));
-        when(testRepository.save(any(TestEntity.class))).thenReturn(test);
     }
 
     /**
@@ -92,6 +107,7 @@ class TestServiceTest {
      */
     @Test
     void testHandleAdd() {
+        when(testRepository.save(any(TestEntity.class))).thenReturn(test);
         String addMessage = testService.handleAdd(userId);
         assertEquals("Введите название теста", addMessage);
 
@@ -100,13 +116,13 @@ class TestServiceTest {
         userSession.setCurrentTest(test);
         userSession.setState(UserState.ADD_TEST_TITLE);
 
-        String addTitleMessage = testService.handleMessage(userSession, "Название теста 2");
+        String addTitleMessage = testService.handleMessage(chatId, userSession, "Название теста 2").getText();
         assertEquals("Введите описание теста", addTitleMessage);
 
         verify(userService).setState(userId, UserState.ADD_TEST_DESCRIPTION);
         userSession.setState(UserState.ADD_TEST_DESCRIPTION);
 
-        String addDescriptionMessage = testService.handleMessage(userSession, "Описание теста 2");
+        String addDescriptionMessage = testService.handleMessage(chatId,userSession, "Описание теста 2").getText();
         assertEquals("Тест “Название теста 2” создан! Количество вопросов: 0. Для добавление вопросов используйте /add_question 123, где 123 - идентификатор теста “Название теста 2”.", addDescriptionMessage);
     }
 
@@ -115,13 +131,15 @@ class TestServiceTest {
      */
     @Test
     void testHandleView() {
-        String result = testService.handleView(userId, "/view");
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        when(testRepository.findById(testId)).thenReturn(Optional.of(test));
+        SendMessage result = testService.handleView(chatId, userId, "/view");
 
-        assertEquals("Выберите тест для просмотра:\n1)  id: 123 Название теста\n", result);
+        assertEquals("Выберите тест для просмотра:\n1)  id: 123 Название теста\n", result.getText());
         verify(userService).setState(userId, UserState.VIEW_TEST);
         userSession.setState(UserState.VIEW_TEST);
 
-        result = testService.handleMessage(userSession, "123");
+        result = testService.handleMessage(chatId, userSession, "123");
         assertEquals("""
                 Тест “Название теста”. Всего вопросов: 1
                 Вопрос: Вопрос
@@ -129,7 +147,7 @@ class TestServiceTest {
                 1 - Ответ
                 Правильный вариант: Ответ
 
-                """, result);
+                """, result.getText());
     }
 
     /**
@@ -137,7 +155,9 @@ class TestServiceTest {
      */
     @Test
     void testHandleViewWithId() {
-        String result = testService.handleView(userId, "/view 123");
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        when(testRepository.findById(testId)).thenReturn(Optional.of(test));
+        SendMessage result = testService.handleView(chatId, userId, "/view 123");
         assertEquals("""
                 Тест “Название теста”. Всего вопросов: 1
                 Вопрос: Вопрос
@@ -145,7 +165,7 @@ class TestServiceTest {
                 1 - Ответ
                 Правильный вариант: Ответ
 
-                """, result);
+                """, result.getText());
     }
 
     /**
@@ -153,9 +173,10 @@ class TestServiceTest {
      */
     @Test
     void testHandleViewNotFound() {
-        String result = testService.handleView(userId, "/view 999");
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        SendMessage result = testService.handleView(chatId, userId, "/view 999");
 
-        assertEquals("Тест не найден!", result);
+        assertEquals("Тест не найден!", result.getText());
         verify(userService).setState(userId, UserState.DEFAULT);
     }
 
@@ -164,8 +185,9 @@ class TestServiceTest {
      */
     @Test
     void testHandleViewInvalidId() {
-        String result = testService.handleView(userId, "/view AAA");
-        assertEquals("Ошибка ввода!", result);
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        SendMessage result = testService.handleView(chatId, userId, "/view AAA");
+        assertEquals("Ошибка ввода!", result.getText());
     }
 
     /**
@@ -173,24 +195,30 @@ class TestServiceTest {
      */
     @Test
     void testHandleEditTitle() {
-        String result = testService.handleEdit(userId, "/edit 123");
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        when(testRepository.findById(testId)).thenReturn(Optional.of(test));
+        when(testRepository.save(any(TestEntity.class))).thenReturn(test);
+        SendMessage result = testService.handleEdit(chatId, userId, "/edit 123");
 
-        assertEquals("""
-                Вы выбрали тест “Название теста”. Что вы хотите изменить?
-                1: Название теста
-                2: Описание теста
-                """, result);
+        assertEquals("Вы выбрали тест “Название теста”. Что вы хотите изменить?” ", result.getText());
         verify(userService).setState(userId, UserState.EDIT_TEST);
         verify(userService).setCurrentTest(userId, test);
         userSession.setCurrentTest(test);
         userSession.setState(UserState.EDIT_TEST);
-        result = testService.handleMessage(userSession, "1");
-        assertEquals("Введите новое название теста", result);
+
+        verify(keyboardService).createReply(
+                eq(List.of("Название теста", "Описание теста")),
+                eq(List.of("changeText 123", "changeDescription 123")),
+                eq("TEST")
+        );
+
+        result = testService.handleMessage(chatId, userSession, "1");
+        assertEquals("Введите новое название теста", result.getText());
 
         verify(userService).setState(userId, UserState.EDIT_TEST_TITLE);
         userSession.setState(UserState.EDIT_TEST_TITLE);
-        result = testService.handleMessage(userSession, "Измененное название теста");
-        assertEquals("Название изменено на “Измененное название теста”", result);
+        result = testService.handleMessage(chatId, userSession, "Измененное название теста");
+        assertEquals("Название изменено на “Измененное название теста”", result.getText());
         verify(userService).setState(userId, UserState.DEFAULT);
     }
 
@@ -199,35 +227,31 @@ class TestServiceTest {
      */
     @Test
     void testHandleEditDescription() {
-        String result = testService.handleEdit(userId, "/edit 123");
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        when(testRepository.findById(testId)).thenReturn(Optional.of(test));
+        when(testRepository.save(any(TestEntity.class))).thenReturn(test);
+        SendMessage result = testService.handleEdit(chatId, userId, "/edit 123");
 
-        assertEquals("""
-                Вы выбрали тест “Название теста”. Что вы хотите изменить?
-                1: Название теста
-                2: Описание теста
-                """, result);
+        assertEquals("Вы выбрали тест “Название теста”. Что вы хотите изменить?” ", result.getText());
         verify(userService).setState(userId, UserState.EDIT_TEST);
         verify(userService).setCurrentTest(userId, test);
         userSession.setCurrentTest(test);
         userSession.setState(UserState.EDIT_TEST);
-        result = testService.handleMessage(userSession, "2");
-        assertEquals("Введите новое описание теста", result);
+
+        verify(keyboardService).createReply(
+                eq(List.of("Название теста", "Описание теста")),
+                eq(List.of("changeText 123", "changeDescription 123")),
+                eq("TEST")
+        );
+
+        result = testService.handleMessage(chatId, userSession, "2");
+        assertEquals("Введите новое описание теста", result.getText());
 
         verify(userService).setState(userId, UserState.EDIT_TEST_DESCRIPTION);
         userSession.setState(UserState.EDIT_TEST_DESCRIPTION);
-        result = testService.handleMessage(userSession, "Новое описание теста");
-        assertEquals("Описание изменено на “Новое описание теста”", result);
+        result = testService.handleMessage(chatId, userSession, "Новое описание теста");
+        assertEquals("Описание изменено на “Новое описание теста”", result.getText());
         verify(userService).setState(userId, UserState.DEFAULT);
-    }
-
-    /**
-     * Тестирование команды /edit [testID] с тестом, которого нет у пользователя
-     */
-    @Test
-    void testHandleEditNotFound() {
-        String result = testService.handleEdit(userId, "/edit 555");
-
-        assertEquals("Тест не найден!", result);
     }
 
     /**
@@ -235,7 +259,8 @@ class TestServiceTest {
      */
     @Test
     void testHandleEditInvalidId() {
-        String result = testService.handleEdit(userId, "/edit aaa");
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        String result = testService.handleEdit(chatId, userId, "/edit aaa").getText();
 
         assertEquals("Ошибка ввода!", result);
     }
@@ -245,20 +270,29 @@ class TestServiceTest {
      */
     @Test
     void testHandleDel() {
-       String result = testService.handleDel(userId);
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        when(testRepository.findById(testId)).thenReturn(Optional.of(test));
+        SendMessage result = testService.handleDel(chatId, userId);
 
-        assertEquals("Выберите тест:\n1)  id: 123 Название теста\n", result);
+        assertEquals("Выберите тест:\n1)  id: 123 Название теста\n", result.getText());
         verify(userService).setState(userId, UserState.DELETE_TEST);
         userSession.setState(UserState.DELETE_TEST);
-        result = testService.handleMessage(userSession, "123");
-        assertEquals("Тест “Название теста” будет удалён, вы уверены? (Да/Нет)", result);
+        result = testService.handleMessage(chatId, userSession, "123");
+        assertEquals("Тест “Название теста” будет удалён, вы уверены?", result.getText());
 
         verify(userService).setState(userId, UserState.CONFIRM_DELETE_TEST);
         verify(userService).setCurrentTest(userId, test);
         userSession.setState(UserState.CONFIRM_DELETE_TEST);
         userSession.setCurrentTest(test);
-        result = testService.handleMessage(userSession, "Да");
-        assertEquals("Тест “Название теста” удалён", result);
+
+        verify(keyboardService).createReply(
+                eq(List.of("Да", "Нет")),
+                eq(List.of("confirmDeleteYes 123", "confirmDeleteNo 123")),
+                eq("TEST")
+        );
+
+        result = testService.handleMessage(chatId, userSession, "Да");
+        assertEquals("Тест “Название теста” удалён", result.getText());
         verify(userService).setState(userId, UserState.DEFAULT);
         verify(testRepository).delete(test);
     }
@@ -268,13 +302,14 @@ class TestServiceTest {
      */
     @Test
     void testHandleDelInvalidId() {
-       String result = testService.handleDel(userId);
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        SendMessage result = testService.handleDel(chatId, userId);
 
-        assertEquals("Выберите тест:\n1)  id: 123 Название теста\n", result);
+        assertEquals("Выберите тест:\n1)  id: 123 Название теста\n", result.getText());
         verify(userService).setState(userId, UserState.DELETE_TEST);
         userSession.setState(UserState.DELETE_TEST);
-        result = testService.handleMessage(userSession, "ффв");
-        assertEquals("Ошибка ввода!", result);
+        result = testService.handleMessage(chatId, userSession, "ффв");
+        assertEquals("Ошибка ввода!", result.getText());
     }
 
     /**
@@ -282,12 +317,14 @@ class TestServiceTest {
      */
     @Test
     void testHandleDelNotFound() {
-        String result = testService.handleDel(userId);
+        when(userService.getTestsById(userId)).thenReturn(List.of(test));
+        SendMessage result = testService.handleDel(chatId, userId);
 
-        assertEquals("Выберите тест:\n1)  id: 123 Название теста\n", result);
+        assertEquals("Выберите тест:\n1)  id: 123 Название теста\n", result.getText());
         verify(userService).setState(userId, UserState.DELETE_TEST);
         userSession.setState(UserState.DELETE_TEST);
-        result = testService.handleMessage(userSession, "555");
-        assertEquals("Тест не найден!", result);
+
+        result = testService.handleMessage(chatId, userSession, "555");
+        assertEquals("Тест не найден!", result.getText());
     }
 }
